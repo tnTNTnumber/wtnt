d941912ebbcbe7be5f19077928d8d50f8b036e10
LeAn <77890558+tnTNTnumber@users.noreply.github.com>
2021-01-28 18:10:45 +0100

    Add files via upload

--- JS/GeneratorFolder.js
+++ JS/GeneratorFolder.js
@@ -2,13 +2,13 @@ function _GeneratorFolder_M_fe(_GeneratorFolder_M_T_ve) { /*Main*/
 	/*Title*/
 	var _GeneratorFolder_M_I_ve = [0,0,0]; /*Iteration*/
 	document.body.innerHTML = '<div class="_D1_ce"></div>';
-	document.getElementsByClassName('_D1_ce')[0].innerHTML = '<h1>'+_GeneratorFolder_M_T_ve+'</h1>';
+	document.getElementsByClassName('_D1_ce')[0].innerHTML = '<h1>'+_GeneratorFolder_M_T_ve+'</h1><table class="_GeneratorFolder_LE_T_ce"></table>';
 	for(_GeneratorFolder_M_I_ve[2]; _GeneratorFolder_M_I_ve[2]<_LA_ve.length-1; _GeneratorFolder_M_I_ve[2]++) {
-		document.getElementsByClassName('_D1_ce')[0].innerHTML += '<p><span><span><a class="_C_ce '+_LA_ve[_GeneratorFolder_M_I_ve[2]][0]+'" href="'+_LA_ve[_GeneratorFolder_M_I_ve[2]][1]+'">'+_LA_ve[_GeneratorFolder_M_I_ve[2]][2]+'</a></span><span class="_FT_ce '+_LA_ve[_GeneratorFolder_M_I_ve[2]][3]+'"></span></span></p>';
+		document.getElementsByClassName('_D1_ce')[0].getElementsByClassName('_GeneratorFolder_LE_T_ce')[0].innerHTML += '<tr class="_GeneratorFolder_LE_CF_ce"><th></th></tr><tr><th class="_GeneratorFolder_LE_C_ce"><p class="_GeneratorFolder_LE_ce"><span><span><a class="_GeneratorFolder_C_ce '+_LA_ve[_GeneratorFolder_M_I_ve[2]][0]+'" href="'+_LA_ve[_GeneratorFolder_M_I_ve[2]][1]+'">'+_LA_ve[_GeneratorFolder_M_I_ve[2]][2]+'</a></span><span class="_GeneratorFolder_FT_ce '+_LA_ve[_GeneratorFolder_M_I_ve[2]][3]+'"></span></span></p></th></tr>';
 	};
 	for(_GeneratorFolder_M_I_ve[1]; _GeneratorFolder_M_I_ve[1]<_FT_ve.length-1; _GeneratorFolder_M_I_ve[1]++) {
-		for(_GeneratorFolder_M_I_ve[0]; _GeneratorFolder_M_I_ve[0]<document.getElementsByClassName('_FT_'+_FT_ve[_GeneratorFolder_M_I_ve[1]][0]+'_ce').length; _GeneratorFolder_M_I_ve[0]++) {
-			document.getElementsByClassName('_FT_'+_FT_ve[_GeneratorFolder_M_I_ve[1]][0]+'_ce')[_GeneratorFolder_M_I_ve[0]].innerHTML = _FT_ve[_GeneratorFolder_M_I_ve[1]][1];
+		for(_GeneratorFolder_M_I_ve[0]; _GeneratorFolder_M_I_ve[0]<document.getElementsByClassName('_GeneratorFolder_FT_'+_FT_ve[_GeneratorFolder_M_I_ve[1]][0]+'_ce').length; _GeneratorFolder_M_I_ve[0]++) {
+			document.getElementsByClassName('_GeneratorFolder_FT_'+_FT_ve[_GeneratorFolder_M_I_ve[1]][0]+'_ce')[_GeneratorFolder_M_I_ve[0]].innerHTML = _FT_ve[_GeneratorFolder_M_I_ve[1]][1];
 		};
 		_GeneratorFolder_M_I_ve[0] = 0;
 	};
